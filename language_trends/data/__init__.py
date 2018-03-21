d2eddbc677b31f47bcb09169402b62a1aa7df361
Oleg Knyazev <knyazev751@gmail.com>
2018-03-22 00:23:30 +0300

    Implement a method for storying a repository

--- language_trends/data/__init__.py
+++ language_trends/data/__init__.py
@@ -2,16 +2,26 @@ from contextlib import contextmanager
 from . import migrations
 from . import access
 
-def store(record):
+def store_repo(id, name, language):
   with _transaction() as c:
     c.execute(
-      'INSERT INTO repositories VALUES (%s, %s, %s);',
-      (record['id'], record['name'], record['language']))
+     '''INSERT INTO repositories VALUES (%s, %s, %s)
+          ON CONFLICT (id) DO UPDATE
+            SET name = excluded.name,
+                language = excluded.language;''',
+      (id, name, language))
+
+def store_commits(repo_id, date, commits_count):
+  with _transaction() as c:
+    c.execute(
+      'INSERT INTO commits_per_date VALUES (%s, %s, %s);',
+      (repo_id, date, commits_count))
 
 _migrated = False
 
 @contextmanager
 def _transaction():
+  global _migrated
   if not _migrated:
     with access.transaction() as c:
       migrations.migrate(c)
