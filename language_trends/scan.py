23fa284b0eb5c663ab55fa924d5a960cf52fe1a5
Oleg Knyazev <knyazev751@gmail.com>
2018-03-25 08:47:30 +0300

    Transform the scanning function into an iterator
    So that it's now easy to perform a limited number of actions.

--- language_trends/scan.py
+++ language_trends/scan.py
@@ -2,9 +2,16 @@ import itertools
 from . import github
 from . import data
 
-def scan_repositories(language):
+def scan_commits(language):
+  """Returns an iterator yielding actions that should be performed to make
+  the database reflect the current GitHub state.
+  """
   for repo_id, repo_name in github.fetch_repos(language):
-    data.store_repo(repo_id, repo_name, language)
+    yield lambda: data.store_repo(repo_id, repo_name, language)
     all_commits = github.fetch_commits(repo_id)
     for date, commits in itertools.groupby(all_commits, key=lambda dt: dt.date()):
-      data.store_commits(repo_id, date, sum(1 for c in commits))
+      yield lambda: data.store_commits(repo_id, date, sum(1 for c in commits))
+
+def perform(actions):
+  for a in actions:
+    a()
