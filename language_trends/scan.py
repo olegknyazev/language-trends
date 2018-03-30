6e1e5163b0691b3579c32e15279018571344467c
Oleg Knyazev <knyazev751@gmail.com>
2018-03-30 12:25:13 +0300

    Parallelize repository scanning
    By using the approach shown in experiment.py.
    
    Also fix a bug causing batched INSERT to fail on repositories having unordered commit history (i.e. on most of repositories?).

--- language_trends/scan.py
+++ language_trends/scan.py
@@ -1,8 +1,10 @@
 import asyncio
-from aitertools import groupby
+from itertools import groupby
 from datetime import datetime, time
 from . import github
 from . import data
+
+MAX_PARALLEL_REPOS = 4
 
 async def _should_update(language):
   scanned = data.repo_count(language)
@@ -11,24 +13,36 @@ async def _should_update(language):
 
 async def _update_impl(language='clojure', log=None):
   if await _should_update(language):
-    async for repo_id, repo_name in github.fetch_repos(language):
-      last_commit = data.last_commit_date(repo_id)
-      if last_commit is None:
-        data.store_repo(repo_id, repo_name, language)
-      else:
-        last_commit = datetime.combine(last_commit, time.min)
-      commits_since_last = github.fetch_commits(repo_id, since=last_commit)
-      commits_by_day = groupby(commits_since_last, key=lambda dt: dt.date())
-      records = [(date, _count(commits)) async for date, commits in commits_by_day]
-      data.store_commits(repo_id, records)
-      if log is not None:
-        log(f'Processed {repo_name}: {len(records)} days, {sum(r[1] for r in records)} commits')
+    await _for_each_parallel(
+      github.fetch_repos(language),
+      lambda r: _update_repo_commits(*r, language, log=log),
+      max_parallelism=MAX_PARALLEL_REPOS)
 
-async def _count(aiter):
-  r = 0
+async def _update_repo_commits(repo_id, repo_name, language, log=None):
+  last_commit = data.last_commit_date(repo_id)
+  if last_commit is None:
+    data.store_repo(repo_id, repo_name, language)
+  else:
+    last_commit = datetime.combine(last_commit, time.min)
+  new_commits = [x async for x in github.fetch_commits(repo_id, since=last_commit)]
+  new_commits.sort(key=lambda dt: dt.date())
+  commits_by_day = groupby(new_commits, key=lambda dt: dt.date())
+  records = [(date, sum(1 for x in commits)) for date, commits in commits_by_day]
+  data.store_commits(repo_id, records)
+  if log is not None:
+    log(f'Processed {repo_name}: {len(records)} days, {sum(r[1] for r in records)} commits')
+
+async def _for_each_parallel(aiter, process, max_parallelism):
+  tasks = {}
+  async def execute(k):
+    await process(k)
+    return k
   async for x in aiter:
-    r += 1
-  return r
+    tasks[x] = asyncio.get_event_loop().create_task(execute(x))
+    if len(tasks) >= max_parallelism:
+      finished = await next(asyncio.as_completed(tasks.values()))
+      tasks.pop(finished)
+  await asyncio.gather(*tasks.values())
 
 def update(language='clojure', log=None):
   loop = asyncio.get_event_loop()
