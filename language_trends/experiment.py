852171ac1c5bffa3174fa295397e07ed0a77a624
Oleg Knyazev <knyazev751@gmail.com>
2018-03-30 12:03:32 +0300

    Extract a method performing 'sliding window' parallel execution
    So that it can be used in the main code.

--- language_trends/experiment.py
+++ language_trends/experiment.py
@@ -17,20 +17,25 @@ async def fetch_commits(repo):
 async def process_repo(r):
   async for c in fetch_commits(r):
     print(c)
-  return r
 
 async def experiment_impl_serial():
   async for r in fetch_repos():
     await process_repo(r)
 
-async def experiment_impl_parallel():
+async def perform(aiter, process, max_parallelism):
   tasks = {}
-  async for r in fetch_repos():
-    tasks[r] = asyncio.get_event_loop().create_task(process_repo(r))
-    if len(tasks) >= 2:
+  async def execute(k):
+    await process(k)
+    return k
+  async for x in aiter:
+    tasks[x] = asyncio.get_event_loop().create_task(execute(x))
+    if len(tasks) >= max_parallelism:
       finished = await next(asyncio.as_completed(tasks.values()))
       tasks.pop(finished)
   await asyncio.gather(*tasks.values())
+
+async def experiment_impl_parallel():
+  await perform(fetch_repos(), process_repo, 2)
 
 def experiment():
   loop = asyncio.get_event_loop()
