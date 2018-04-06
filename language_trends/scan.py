5f1a527feacc58b1d52a56611d1c63d73a0ffd99
Oleg Knyazev <knyazev751@gmail.com>
2018-04-06 15:29:34 +0300

    Remove unused code from scan and restore automatic refreshing of the aggregated data

--- language_trends/scan.py
+++ language_trends/scan.py
@@ -1,6 +1,4 @@
 import asyncio
-from itertools import groupby
-from datetime import datetime, time
 
 from dateutil.parser import parse as parse_date
 
@@ -47,31 +45,12 @@ async def _update_impl(language, log=None):
 def update_aggregated_data():
   data.update_aggregated_data()
 
-async def _should_update(github, language):
-  scanned = data.repo_count(language)
-  total = await github.repo_count(language)
-  return scanned < total
-
-async def _update_repo_commits(github, repo_id, repo_name, language, log=None):
-  last_commit = data.last_commit_date(repo_id)
-  if last_commit is None:
-    data.store_repo(repo_id, repo_name, language)
-  else:
-    last_commit = datetime.combine(last_commit, time.min)
-  new_commits = [x async for x in github.fetch_commits(repo_id, since=last_commit)]
-  new_commits.sort(key=lambda dt: dt.date())
-  commits_by_day = groupby(new_commits, key=lambda dt: dt.date())
-  records = [(date, sum(1 for x in commits)) for date, commits in commits_by_day]
-  data.store_commits(repo_id, records)
-  if log is not None:
-    log(f'Processed {repo_name}: {len(records)} days, {sum(r[1] for r in records)} commits')
-
 def main():
   try:
     update_language('clojure')
   except KeyboardInterrupt:
     pass
-  # update_aggregated_data()
+  update_aggregated_data()
 
 if __name__ == '__main__':
   main()
