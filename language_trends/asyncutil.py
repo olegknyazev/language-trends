88ad59d24e501a3b9edbb7e1950fa92aa3cac5bd
Oleg Knyazev <knyazev751@gmail.com>
2018-04-06 14:37:12 +0300

    Make repositories scanning parallel again

--- language_trends/asyncutil.py
+++ language_trends/asyncutil.py
@@ -4,9 +4,9 @@ async def for_each_parallel(aiter, process, max_parallelism):
   tasks = {}
   async def execute(item):
     await process(item)
-    return item
+    return id(item)
   async for item in aiter:
-    tasks[item] = asyncio.get_event_loop().create_task(execute(item))
+    tasks[id(item)] = asyncio.get_event_loop().create_task(execute(item))
     if len(tasks) >= max_parallelism:
       finished = await next(asyncio.as_completed(tasks.values()))
       tasks.pop(finished)
--- language_trends/scan.py
+++ language_trends/scan.py
@@ -6,7 +6,10 @@ from dateutil.parser import parse as parse_date
 
 from .github import Session as GitHubSession
 from .languages import ALL_LANGUAGES
+from .asyncutil import for_each_parallel
 from . import data
+
+MAX_PARALLEL_REPOS = 4
 
 def update_all(log=None):
   loop = asyncio.get_event_loop()
@@ -20,14 +23,17 @@ def update_language(language, log=None):
   loop.close()
 
 async def _update_impl(language, log=None):
+  async def process_repo(repo):
+    commits = (
+      await github.fetch_commits_monthly_breakdown(
+        repo['id'],
+        since=parse_date(repo['createdAt']),
+        until=parse_date(repo['pushedAt'])))
+    print(repo['id'], repo['name'], sum(x[1] for x in commits))
+
   async with GitHubSession() as github:
-    async for repo in github.fetch_repos(language, ['id', 'name', 'createdAt', 'pushedAt']):
-      commits = (
-        await github.fetch_commits_monthly_breakdown(
-          repo['id'],
-          since=parse_date(repo['createdAt']),
-          until=parse_date(repo['pushedAt'])))
-      print(repo['id'], repo['name'], sum(x[1] for x in commits))
+    repos = github.fetch_repos(language, ['id', 'name', 'createdAt', 'pushedAt'])
+    await for_each_parallel(repos, process_repo, MAX_PARALLEL_REPOS)
 
 def update_aggregated_data():
   data.update_aggregated_data()
