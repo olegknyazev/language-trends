337ec752c5be24f5966f1d7c1af00054002190b0
Oleg Knyazev <knyazev751@gmail.com>
2018-04-16 16:45:59 +0300

    Display on the chart all known languages that have some commits

--- language_trends/ui/__init__.py
+++ language_trends/ui/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 from flask.json import JSONEncoder, dumps
 
 from language_trends import data
+from language_trends.languages import ALL_LANGUAGES
 
 class CustomJSONEncoder(JSONEncoder):
   def default(self, obj):
@@ -22,8 +23,8 @@ def _collect_data(langs):
       total += commits
       yield date, total
 
-  def data_for_lang(lang):
-    return list()
+  def remove_indices(lst, indices):
+    return [x for i, x in enumerate(lst) if i not in indices]
 
   num_langs = len(langs)
   result = defaultdict(lambda: [0] * num_langs)
@@ -31,13 +32,16 @@ def _collect_data(langs):
     for date, commits in integrate(data.commits_by_language(lang)):
       result[date][i] = commits
 
+  empty_languages = {i for i in range(num_langs) if all(c[i] == 0 for c in result.values())}
+  langs = remove_indices(langs, empty_languages)
+  result = {k: remove_indices(v, empty_languages) for k, v in result.items()}
+
   commits = [[k, *v] for k, v in result.items()]
   commits.sort(key=lambda x: x[0])
   return {'langs': langs, 'commits': commits}
 
 @app.route('/')
 def index():
-  langs = ['scala', 'python']
   return render_template(
     'index.html',
-    data=dumps(_collect_data(langs)))
+    data=dumps(_collect_data(ALL_LANGUAGES)))
